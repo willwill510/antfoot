4d64df817465e94d80e7d5c8d3d542671970b00a
William <138241936+willwill510@users.noreply.github.com>
2025-06-12 08:32:43 -0400

    added sounds (WIP) & sprinting

--- player.gd
+++ player.gd
@@ -1,15 +1,31 @@
 extends CharacterBody3D
 
 const MOVE_SPEED = 4
+const SPRINT_SPEED = 6
+const STAMINA_REGEN = 0.25
+const MOVE_PITCH = 1
+const SPRINT_PITCH = 2
+const MAX_STAMINA = 6
 const MOUSE_SENS = 0.5
 const SPRITE_SCALE = Vector2(4, 4)
 
 @onready var scene_manager = get_parent().get_parent()
+@onready var breathing = $Breathing
+@onready var footsteps_inside = $FootstepsInside
+@onready var footsteps_outside = $FootstepsOutside
+
+var sprinting = false
+var exausted = false
+var stamina = MAX_STAMINA
 
 var focus = 'world'
 var equipped = false
 
+var footstep_audio
+
 func _ready() -> void:
+	breathing.play()
+	footstep_audio = footsteps_inside
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	await get_tree().process_frame
 	get_tree().call_group('zombies', 'set_player', self)
@@ -52,15 +68,64 @@ func _physics_process(delta: float) -> void:
 			move_vec.x -= 1
 		if Input.is_action_pressed('move_right'):
 			move_vec.x += 1
+		
 		move_vec = move_vec.normalized()
 		move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
-		move_and_collide(move_vec * MOVE_SPEED * delta)
+		var speed = 0
+		
+		breathing.volume_db = linear_to_db(1 - (stamina / MAX_STAMINA))
+		
+		if is_outside():
+			footstep_audio = footsteps_outside
+		else:
+			footstep_audio = footsteps_inside
+		
+		if sprinting:
+			speed = SPRINT_SPEED
+		else:
+			speed = MOVE_SPEED
+		
+		if move_vec != Vector3(0, 0, 0):
+			if not footstep_audio.playing:
+				footstep_audio.play()
+			
+		elif footstep_audio.playing:
+			footstep_audio.stop()
+		
+		move_and_collide(move_vec * speed * delta)
+		
+		if Input.is_action_pressed('sprint'):
+			if not exausted:
+				if not sprinting:
+					sprinting = true
+					footstep_audio.pitch_scale = SPRINT_PITCH
+				stamina = stamina - delta
+				
+				if stamina <= 0:
+					sprinting = false
+					footstep_audio.pitch_scale = MOVE_PITCH
+					exausted = true
+		else:
+			if sprinting:
+				sprinting = false
+				footstep_audio.pitch_scale = MOVE_PITCH
+			
+			if stamina < MAX_STAMINA:
+				stamina = stamina + (delta * STAMINA_REGEN)
+			elif exausted:
+				exausted = false
 		
 		#if Input.is_action_pressed('use') and !anim_player.is_playing():
 			#anim_player.play('use_flare')
 			#var coll = raycast.get_collider()
 			#if raycast.is_colliding() and coll.has_method('kill'):
 				#coll.kill()
-#
+
 func kill() -> void:
 	get_tree().reload_current_scene()
+
+func is_outside() -> bool:
+	if scene_manager.current_scene.get_name() == 'World':
+		return true
+	
+	return false
